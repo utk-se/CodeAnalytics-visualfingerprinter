c5b63d77e2e1c48147ec797826db086b11d4acbf
Ben Klein <robobenklein@gmail.com>
2020-10-22 07:15:07 -0400

    performance improved by splitting module per-proc

--- cafingerprinter/__main__.py
+++ cafingerprinter/__main__.py
@@ -22,22 +22,31 @@ def analyze(repopath, modules=None, per_file_results=False):
                 log.error(f"Could not find module {m}")
                 log.error(e)
                 raise
+    log.debug(f"Modules: {_m_classes}")
 
     abspath = os.path.abspath(repopath)
+    log.debug(f"Repo path {abspath}")
     with utils.pushd(abspath):
         return cafpmodules.run_modules(_m_classes, abspath, per_file_results)
 
 def main():
     parser = argparse.ArgumentParser(description="CA-VisualFingerprinter")
-    parser.add_argument('repopath', metavar='repo', type=str, help="Path to repo to analyze")
-    parser.add_argument('--json', metavar='json_outfile', type=str, help="Path to write result json")
-    parser.add_argument('--quiet', '-q', action='store_true', help="Reduce logging verbosity and don't print output.")
+    parser.add_argument('repopath', metavar='./path/to/repo', type=str, help="Path to repo to analyze")
+    parser.add_argument('--json', metavar='results.json', type=str, help="Path to write result json")
+    parser.add_argument('--quiet', '-q', action='store_true', help="Reduce logging verbosity and don't print the result.")
+    parser.add_argument('--verbose', '-v', action='store_true', help="Enable debug logging.")
     parser.add_argument('--perfile', dest='per_file_results', action='store_true', help="Include per-file results in output json.")
 
     which_modules = parser.add_mutually_exclusive_group()
     which_modules.add_argument('--skip', metavar='module_name', dest='skip_modules', action='append', help="Skip an analysis module by name")
     which_modules.add_argument('--only', metavar='module_name', dest='only_modules', action='append', help="Only run specified analysis module(s)")
     args = parser.parse_args()
+
+    if args.quiet:
+        log.setLevel(log.WARN)
+    if args.verbose:
+        log.setLevel(log.DEBUG)
+        log.debug("Debug output enabled.")
 
     modules = cafpmodules._friendly_names.values()
     if args.skip_modules:
@@ -51,8 +60,12 @@ def main():
     log.info(f"Running analyzer on {args.repopath}")
     r = analyze(args.repopath, modules, per_file_results=args.per_file_results)
 
-    if 'json_outfile' not in args and not args.quiet:
+    if not args.quiet:
         log.info(f"Analysis output:\n{json.dumps(r, indent=2, sort_keys=True)}")
+
+    if 'json_outfile' in args:
+        with open(args.json_outfile, 'w') as f:
+            f.write(json.dumps)
 
     r_bson = dumps(r)
     log.info(f"Result BSON size: {len(r_bson)}")
